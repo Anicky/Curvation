f63b2d97c195116727aa2c681a67f43cdb5be4c6
Anicky <jeremie.jalouzet@gmail.com>
2015-08-28 20:14:35 +0200

    [Common] Utilisation de MainLoop.js pour meilleure gestion des FPS et du game loop, ajout d'un deuxième joueur (touches S et D)

--- js/Player.js
+++ js/Player.js
@@ -5,8 +5,12 @@ function Player(name, color) {
 }
 
 Player.prototype.init = function () {
+    this.keyPressedLeft = 0;
+    this.keyPressedRight = 0;
     this.x = setRandomX();
     this.y = setRandomY();
+    this.lastX = this.x;
+    this.lastY = this.y;
     this.direction = 1;
     this.speed = DEFAULT_SNAKE_SPEED;
     this.size = DEFAULT_SNAKE_SIZE;
@@ -16,4 +20,60 @@ Player.prototype.init = function () {
     this.holeProbability = DEFAULT_SNAKE_HOLE_PROBABILITY;
     this.holeTimer = 0;
     this.currentHole = false;
+    this.collisionsCheck = false;
+}
+
+Player.prototype.update = function (delta) {
+    if (noCollisionsTimer >= DEFAULT_NO_COLLISIONS_TIME) {
+        if (this.currentHole) {
+            this.holeTimer++;
+            if (this.holeTimer > this.holeSize) {
+                this.holeTimer = 0;
+                this.currentHole = false;
+            }
+        } else if (this.holeTimer > this.holeMinimumTime) {
+            var holeProbability = Math.random() * 100;
+            if (holeProbability < this.holeProbability) {
+                this.currentHole = true;
+                this.holeTimer = 0;
+            } else {
+                this.holeTimer++;
+            }
+        } else {
+            this.holeTimer++;
+        }
+    }
+    this.lastX = this.x;
+    this.lastY = this.y;
+    this.x += Math.cos(this.direction) * (this.speed * delta);
+    this.y += Math.sin(this.direction) * (this.speed * delta);
+    var tempX = this.x;
+    var tempY = this.y;
+    checkKey(this);
+    tempX += Math.cos(this.direction) * this.size;
+    tempY += Math.sin(this.direction) * this.size;
+    if (noCollisionsTimer < DEFAULT_NO_COLLISIONS_TIME) {
+        noCollisionsTimer++;
+    } else if (!this.currentHole) {
+        this.collisionsCheck = this.checkCollisions(tempX, tempY);
+    }
+};
+
+Player.prototype.draw = function (interpolationPercentage) {
+    if (!this.currentHole) {
+        var x = this.lastX + (this.x - this.lastX) * interpolationPercentage;
+        var y = this.lastY + (this.y - this.lastY) * interpolationPercentage;
+        context.fillStyle = this.color;
+        context.beginPath();
+        context.arc(x, y, this.size, 0, 2 * Math.PI);
+        context.fill();
+    }
+};
+
+Player.prototype.checkCollisions = function (tempX, tempY) {
+    var pixelData = context.getImageData(tempX, tempY, 1, 1);
+    if (this.x <= 0 || this.x >= canvas.width || this.y <= 0 || this.y >= canvas.height || (pixelData.data[3] > 0)) {
+        return true;
+    }
+    return false;
 }
--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
-const KEY_LEFT = 37;
-const KEY_RIGHT = 39;
+const KEY_PLAYER1_LEFT = 37;
+const KEY_PLAYER1_RIGHT = 39;
+const KEY_PLAYER2_LEFT = 83;
+const KEY_PLAYER2_RIGHT = 68;
 
 const DEFAULT_SNAKE_SIZE = 3;
-const DEFAULT_SNAKE_SPEED = 2;
+const DEFAULT_SNAKE_SPEED = 0.1;
 const DEFAULT_SNAKE_CURVE = 3;
 
 const DEFAULT_BEGIN_PADDING = 30;
@@ -15,78 +17,8 @@ const DEFAULT_SNAKE_HOLE_PROBABILITY = 0.5;
 var players = new Array();
 var canvas = null;
 var context = null;
-var keyPressedLeft = 0;
-var keyPressedRight = 0;
 var gameRunning = true;
 var noCollisionsTimer = 0;
-
-function updateCanvas() {
-    var collisionsCheck = false;
-    if (noCollisionsTimer < DEFAULT_NO_COLLISIONS_TIME) {
-        context.clearRect(0, 0, canvas.width, canvas.height);
-    }
-    for (var i = 0; i < players.length; i++) {
-        var p = players[i];
-        if (noCollisionsTimer >= DEFAULT_NO_COLLISIONS_TIME) {
-            if (p.currentHole) {
-                p.holeTimer++;
-                if (p.holeTimer > p.holeSize) {
-                    p.holeTimer = 0;
-                    p.currentHole = false;
-                }
-            } else if (p.holeTimer > p.holeMinimumTime) {
-                var holeProbability = Math.random() * 100;
-                if (holeProbability < p.holeProbability) {
-                    p.currentHole = true;
-                    p.holeTimer = 0;
-                } else {
-                    p.holeTimer++;
-                }
-            } else {
-                p.holeTimer++;
-            }
-        }
-        p.x += Math.cos(p.direction) * p.speed;
-        p.y += Math.sin(p.direction) * p.speed;
-        var tempX = p.x;
-        var tempY = p.y;
-        checkKey();
-        tempX += Math.cos(p.direction) * p.size;
-        tempY += Math.sin(p.direction) * p.size;
-        context.fillStyle = p.color;
-        if (noCollisionsTimer < DEFAULT_NO_COLLISIONS_TIME) {
-            noCollisionsTimer++;
-        } else if (!p.currentHole) {
-            collisionsCheck = checkCollisions(p, tempX, tempY);
-        }
-        if (!p.currentHole) {
-            context.beginPath();
-            context.arc(p.x, p.y, p.size, 0, 2 * Math.PI);
-            context.fill();
-        }
-    }
-    if (collisionsCheck) {
-        $('#gameover').modal('show');
-    }
-    return collisionsCheck;
-}
-
-function checkCollisions(p, tempX, tempY) {
-    var pixelData = context.getImageData(tempX, tempY, 1, 1);
-    if (p.x <= 0 || p.x >= canvas.width || p.y <= 0 || p.y >= canvas.height || (pixelData.data[3] > 0)) {
-        return true;
-    }
-    return false;
-}
-
-function refresh() {
-    setTimeout(function () {
-        gameRunning = updateCanvas();
-        if (!gameRunning) {
-            refresh();
-        }
-    }, 10);
-}
 
 function setRandomX() {
     return Math.floor(Math.random() * (canvas.width - DEFAULT_BEGIN_PADDING * 2)) + DEFAULT_BEGIN_PADDING;
@@ -96,14 +28,46 @@ function setRandomY() {
     return Math.floor(Math.random() * (canvas.height - DEFAULT_BEGIN_PADDING * 2)) + DEFAULT_BEGIN_PADDING;
 }
 
+function update(delta) {
+    if (gameRunning) {
+        if (noCollisionsTimer < DEFAULT_NO_COLLISIONS_TIME) {
+            context.clearRect(0, 0, canvas.width, canvas.height);
+        }
+        for (var i = 0; i < players.length; i++) {
+            var p = players[i];
+            p.update(delta);
+            if (p.collisionsCheck) {
+                gameRunning = false;
+                break;
+            }
+        }
+        if (!gameRunning) {
+            $('#gameover').modal('show');
+        }
+    }
+}
+
+function draw(interpolationPercentage) {
+    for (var i = 0; i < players.length; i++) {
+        players[i].draw(interpolationPercentage);
+    }
+}
+
+function end(fps, panic) {
+    if (panic) {
+        var discardedTime = Math.round(MainLoop.resetFrameDelta());
+        console.warn('Main loop panicked, probably because the browser tab was put in the background. Discarding ' + discardedTime + 'ms');
+    }
+}
+
 function init() {
     context.clearRect(0, 0, canvas.width, canvas.height);
     for (var i = 0; i < players.length; i++) {
         players[i].init();
     }
     gameRunning = true;
-    noCollisionsTimer = 0
-    refresh();
+    noCollisionsTimer = 0;
+    MainLoop.setUpdate(update).setDraw(draw).setEnd(end).start();
 }
 
 $(document).ready(function () {
@@ -111,23 +75,36 @@ $(document).ready(function () {
     canvas.width = $('.panel-body').width();
     canvas.height = $('.panel-body').height();
     context = canvas.getContext("2d");
-    players.push(new Player("Jérémie", 'pink'));
+    players.push(new Player("Player 1", 'red'));
+    players.push(new Player("Player 2", 'blue'));
     $(this).keydown(function (e) {
         e.preventDefault();
-        if (e.keyCode == KEY_LEFT) {
-            keyPressedLeft = 1;
+        if (e.keyCode == KEY_PLAYER1_LEFT) {
+            players[0].keyPressedLeft = 1;
         }
-        else if (e.keyCode == KEY_RIGHT) {
-            keyPressedRight = 1;
+        if (e.keyCode == KEY_PLAYER1_RIGHT) {
+            players[0].keyPressedRight = 1;
+        }
+        if (e.keyCode == KEY_PLAYER2_LEFT) {
+            players[1].keyPressedLeft = 1;
+        }
+        if (e.keyCode == KEY_PLAYER2_RIGHT) {
+            players[1].keyPressedRight = 1;
         }
     });
     $(this).keyup(function (e) {
         e.preventDefault();
-        if (e.keyCode == KEY_LEFT) {
-            keyPressedLeft = 0;
+        if (e.keyCode == KEY_PLAYER1_LEFT) {
+            players[0].keyPressedLeft = 0;
         }
-        else if (e.keyCode == KEY_RIGHT) {
-            keyPressedRight = 0;
+        if (e.keyCode == KEY_PLAYER1_RIGHT) {
+            players[0].keyPressedRight = 0;
+        }
+        if (e.keyCode == KEY_PLAYER2_LEFT) {
+            players[1].keyPressedLeft = 0;
+        }
+        if (e.keyCode == KEY_PLAYER2_RIGHT) {
+            players[1].keyPressedRight = 0;
         }
     });
     $('#retry').click(function () {
@@ -137,12 +114,11 @@ $(document).ready(function () {
     init();
 });
 
-function checkKey() {
-    var p = players[0];
-    if (keyPressedLeft) {
+function checkKey(p) {
+    if (p.keyPressedLeft) {
         p.direction -= p.curve * (Math.PI / 180);
     }
-    else if (keyPressedRight) {
+    else if (p.keyPressedRight) {
         p.direction += p.curve * (Math.PI / 180);
     }
 }
