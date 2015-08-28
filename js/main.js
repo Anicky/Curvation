96b3b45cca3901ae7f5f02bcb8e98997a1bfb835
Smourph <julien.de.conti@gmail.com>
2015-08-28 14:27:14 +0200

    [Common] Mise en place de constantes et CamelCase

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
 const KEY_LEFT = 37;
 const KEY_RIGHT = 39;
 
+const DEFAULT_SNAKE_SIZE = 3;
+const DEFAULT_SNAKE_SPEED = 2;
+const DEFAULT_SNAKE_CURVE = 3;
+
 var players = new Array();
 var canvas = null;
 var context = null;
-var key_pressed_left = 0;
-var key_pressed_right = 0;
-var game_running = true;
-var snake_size = 3;
-var snake_speed = 2;
-var snake_curve = 3;
+var keyPressedLeft = 0;
+var keyPressedRight = 0;
+var gameRunning = true;
 
 function Player(name, color, x, y) {
     this.name = name;
@@ -17,33 +18,33 @@ function Player(name, color, x, y) {
     this.x = x;
     this.y = y;
     this.direction = 1;
-    this.speed = snake_speed;
-    this.size = snake_size;
+    this.speed = DEFAULT_SNAKE_SPEED;
+    this.size = DEFAULT_SNAKE_SIZE;
 }
 
 function updateCanvas() {
-    var collisions_check = false;
+    var collisionsCheck = false;
     for (var i = 0; i < players.length; i++) {
         var p = players[i];
-        var dX = Math.cos(p.direction) * snake_speed;
-        var dY = Math.sin(p.direction) * snake_speed;
+        var dX = Math.cos(p.direction) * DEFAULT_SNAKE_SPEED;
+        var dY = Math.sin(p.direction) * DEFAULT_SNAKE_SPEED;
         p.x += dX;
         p.y += dY;
         var tempX = p.x;
         var tempY = p.y;
         checkKey();
-        tempX += Math.cos(p.direction) * snake_size;
-        tempY += Math.sin(p.direction) * snake_size;
+        tempX += Math.cos(p.direction) * DEFAULT_SNAKE_SIZE;
+        tempY += Math.sin(p.direction) * DEFAULT_SNAKE_SIZE;
         context.fillStyle = p.color;
-        collisions_check = checkCollisions(p, tempX, tempY);
+        collisionsCheck = checkCollisions(p, tempX, tempY);
         context.beginPath();
-        context.arc(p.x, p.y, snake_size, 0, 2 * Math.PI);
+        context.arc(p.x, p.y, DEFAULT_SNAKE_SIZE, 0, 2 * Math.PI);
         context.fill();
     }
-    if (collisions_check) {
+    if (collisionsCheck) {
         $('#gameover').modal('show');
     }
-    return collisions_check;
+    return collisionsCheck;
 }
 
 function checkCollisions(p, tempX, tempY) {
@@ -56,8 +57,8 @@ function checkCollisions(p, tempX, tempY) {
 
 function refresh() {
     setTimeout(function () {
-        game_running = updateCanvas();
-        if (!game_running) {
+        gameRunning = updateCanvas();
+        if (!gameRunning) {
             refresh();
         }
     }, 10);
@@ -70,26 +71,26 @@ $(document).ready(function () {
     context = canvas.getContext("2d");
 	
 	// Position aléatoire
-	random_x = Math.floor(Math.random() * canvas.width - 30) + 1;
-	random_y = Math.floor(Math.random() * canvas.height - 30) + 1;
+	randomX = Math.floor(Math.random() * canvas.width - 30) + 1;
+	randomY = Math.floor(Math.random() * canvas.height - 30) + 1;
 	
-    players.push(new Player("Jérémie", 'pink', random_x, random_y));
+    players.push(new Player("Jérémie", 'pink', randomX, randomY));
     $(this).keydown(function (e) {
         e.preventDefault();
         if (e.keyCode == KEY_LEFT) {
-            key_pressed_left = 1;
+            keyPressedLeft = 1;
         }
         else if (e.keyCode == KEY_RIGHT) {
-            key_pressed_right = 1;
+            keyPressedRight = 1;
         }
     });
     $(this).keyup(function (e) {
         e.preventDefault();
         if (e.keyCode == KEY_LEFT) {
-            key_pressed_left = 0;
+            keyPressedLeft = 0;
         }
         else if (e.keyCode == KEY_RIGHT) {
-            key_pressed_right = 0;
+            keyPressedRight = 0;
         }
     });
     refresh();
@@ -102,21 +103,21 @@ function retry() {
     $('#gameover').modal('hide');
     context.clearRect(0, 0, canvas.width, canvas.height);
 	// Position aléatoire
-	random_x = Math.floor(Math.random() * canvas.width - 30) + 1;
-	random_y = Math.floor(Math.random() * canvas.height - 30) + 1;
-    players[0].x = random_x;
-    players[0].y = random_y;
+	randomX = Math.floor(Math.random() * canvas.width - 30) + 1;
+	randomY = Math.floor(Math.random() * canvas.height - 30) + 1;
+    players[0].x = randomX;
+    players[0].y = randomY;
     players[0].direction = 1;
-    game_running = true;
+    gameRunning = true;
     refresh();
 }
 
 
 function checkKey() {
-    if (key_pressed_left) {
-        players[0].direction -= snake_curve * (Math.PI / 180);
+    if (keyPressedLeft) {
+        players[0].direction -= DEFAULT_SNAKE_CURVE * (Math.PI / 180);
     }
-    else if (key_pressed_right) {
-        players[0].direction += snake_curve * (Math.PI / 180);
+    else if (keyPressedRight) {
+        players[0].direction += DEFAULT_SNAKE_CURVE * (Math.PI / 180);
     }
 }
