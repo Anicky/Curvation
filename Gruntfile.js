25a38fad19b2ccbc314e12a4389969ca8a7af640
Smourph <julien.de.conti@gmail.com>
2015-10-04 09:49:34 +0200

    [Project] fix client src folder for concat and uglify in Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,7 +17,7 @@ module.exports = function (grunt) {
         paths: {
             src: {
                 folder: 'src/',
-                js: '<%= paths.src.folder %>js/**/*.js',
+                js: ['<%= paths.src.folder %>js/shared/*.js', '<%= paths.src.folder %>js/client/*.js'],
                 css: '<%= paths.src.folder %>css/**/*.css',
                 scss: '<%= paths.src.folder %>sass/**/*.scss',
                 img: '<%= paths.src.folder %>img/**/*.png',
