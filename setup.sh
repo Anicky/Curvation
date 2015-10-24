03bbcc80848d3bc779803b80bc88daf4822a74fa
Anicky <jeremie.jalouzet@gmail.com>
2015-10-24 12:31:40 +0200

    [Config] Refactoring commands in setup.sh

--- setup.sh
+++ setup.sh
@@ -6,17 +6,11 @@ echo "*******************************"
 
 cd curvation
 sudo apt-get -qq update
-sudo apt-get -y -qq install build-essential
-sudo apt-get -y -qq install nodejs-legacy
-sudo apt-get -y -qq install npm
-sudo apt-get -y -qq install ruby
-sudo apt-get -y -qq install git
+sudo apt-get -y -qq install nodejs build-essential npm ruby git
 sudo ln -s "$(which nodejs)" /usr/sbin/node
-sudo npm install -g bower
-sudo npm install -g grunt-cli
-sudo npm install -g nodemon
+sudo npm install -g bower grunt-cli nodemon
 sudo gem install sass
-sudo npm install --no-bin-links
+sudo npm install
 bower install
 grunt init
 
