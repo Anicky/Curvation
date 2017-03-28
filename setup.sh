6c6a070e92bfd392e74a0e9029e9325bea4dc7f6
Jérémie JALOUZET <jjalouzet@micropole.com>
2017-03-28 13:35:40 +0200

    [Config] Dependencies updated

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ cd /var/www/curvation
 ln -s /home/vagrant/node_modules/ node_modules
 ln -s /home/vagrant/bower_components/ bower_components
 sudo apt-get -qq update
-sudo apt-get -y -qq install nodejs build-essential npm ruby git
+sudo apt-get -y -qq install nodejs build-essential npm libssl-dev ruby git
 sudo ln -s /usr/bin/nodejs /usr/bin/node
 sudo npm install -g bower grunt-cli nodemon
 sudo gem install sass
