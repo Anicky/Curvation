3b886e8641ad65a6f3a4d521c98c3973086c4cd1
Anicky <jeremie.jalouzet@gmail.com>
2017-07-09 15:45:46 +0200

    [Config] Replace boot.sh by iptables-persistent

--- Vagrantfile
+++ Vagrantfile
@@ -64,9 +64,9 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
     v.cpus = vconfig['vagrant_cpus']
   end
 
-  config.vm.provision "file", source: "app", destination: "/tmp/curvation"
+  config.vm.provision "file", source: "app/Gruntfile.js", destination: "/tmp/curvation/Gruntfile.js"
+  config.vm.provision "file", source: "app/package.json", destination: "/tmp/curvation/package.json"
   config.vm.provision "shell", privileged: false, path: "vagrant/setup.sh"
-  config.vm.provision "shell", privileged: false, path: "vagrant/boot.sh", run: 'always'
 
   # Set the name of the VM. See: http://stackoverflow.com/a/17864388/100134
   config.vm.define vconfig['vagrant_machine_name'] do |d|
--- vagrant/setup.sh
+++ vagrant/setup.sh
@@ -4,16 +4,20 @@ echo "*******************************"
 echo "Provisioning virtual machine..."
 echo "*******************************" 
 
+echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
+echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
 sudo apt-get -qq update
-sudo apt-get -qq -y install curl gnupg
+sudo apt-get -qq -y install curl gnupg iptables-persistent
+sudo iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 3000
+sudo bash -c "iptables-save > /etc/iptables/rules.v4"
 sudo curl -sL https://deb.nodesource.com/setup_8.x | sudo bash -
 sudo apt-get -qq -y install nodejs
 sudo npm install -g grunt-cli nodemon browserify
 mkdir -p /var/www/curvation
 sudo chown -R vagrant:vagrant /var/www/curvation
-rm -r /tmp/curvation/src
-rm -r /tmp/curvation/logs
 mv /tmp/curvation/* /var/www/curvation
+cd /var/www/curvation
+npm install
 
 echo "*********************************"
 echo "Success!"
